9cee7dc6775dcba5303027e5e745e9a0a8d4428c
Ilya Mizus <bigfatbrowncat@gmail.com>
2022-10-28 20:06:53 +0300

    msys2-based mingw-w64 clang toolchain added

---
+++ win-x86_64-build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pacman -S mingw-w64-clang-x86_64-clang mingw-w64-clang-x86_64-libjpeg-turbo mingw-w64-clang-x86_64-libpng \
+          mingw-w64-clang-x86_64-harfbuzz mingw-w64-clang-x86_64-icu mingw-w64-clang-x86_64-libwebp mingw-w64-clang-x86_64-ninja
---
+++ win-x86_64-env.bat
@@ -0,0 +1,2 @@
+set CUR_PATH=%~dp0
+set PATH=%PATH%;%CUR_PATH%\win-x86_64\bin
