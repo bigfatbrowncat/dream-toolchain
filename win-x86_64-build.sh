ecee7b95a48f9fad5541278d93d663f137915a22
Ilya Mizus <bigfatbrowncat@gmail.com>
2022-10-30 01:15:31 +0300

    Added cmake, gdb and glfw

--- win-x86_64-build.sh
+++ win-x86_64-build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-pacman -S mingw-w64-clang-x86_64-clang mingw-w64-clang-x86_64-libjpeg-turbo mingw-w64-clang-x86_64-libpng \
-          mingw-w64-clang-x86_64-harfbuzz mingw-w64-clang-x86_64-icu mingw-w64-clang-x86_64-libwebp mingw-w64-clang-x86_64-ninja
+pacman -S mingw-w64-clang-x86_64-clang \
+          mingw-w64-clang-x86_64-libjpeg-turbo \
+		  mingw-w64-clang-x86_64-libpng \
+          mingw-w64-clang-x86_64-harfbuzz \
+		  mingw-w64-clang-x86_64-icu \
+		  mingw-w64-clang-x86_64-libwebp \
+		  mingw-w64-clang-x86_64-ninja \
+		  mingw-w64-clang-x86_64-cmake \
+		  mingw-w64-clang-x86_64-gdb \
+          mingw-w64-clang-x86_64-glfw
